9322f56d3ebce6852c9592f29bd1ca9b370450f0
joshuajipp <joshuajipp@gmail.com>
2023-04-07 15:18:21 -0600

    fixed sorting algorithm and added methods

--- datastructures/linear/SLL.py
+++ datastructures/linear/SLL.py
@@ -40,36 +40,80 @@ class SinglyLinkedList:
             current.next = node
             self.size += 1
 
-    def sort(self):
-        if self.size > 1:
-            current = self.head.next
-            while current is not None:
-                node_to_insert = current
-                current = current.next
-                self.sorted_insert(node_to_insert)
-
-    def sorted_insert(self, node: Node):
-        if self.head is None:
-            self.insert_head(node)
-            return
-        elif node.val < self.head.val:
-            self.insert_head(node)
-            return
-        elif node.val >= self.tail.val:
-            self.insert_tail(node)
-            return
-        else:
-            current = self.head
-            while current.next.val <= node.val:
-                current = current.next
-            node.next = current.next
-            current.next = node
-            self.size += 1
-
     def search(self, node: Node):
         current = self.head
         while current is not None:
-            if current.data == node.data:
+            if current.val == node.val:
                 return current
             current = current.next
         return None
+
+    def delete_head(self):
+        if self.head is None:
+            return
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+        self.size -= 1
+
+    def delete_tail(self):
+        if self.tail is None:
+            return
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        else:
+            current = self.head
+            while current.next != self.tail:
+                current = current.next
+            current.next = None
+            self.tail = current
+        self.size -= 1
+
+    def delete_node(self, node):
+        if self.head is None:
+            return
+        if self.head == node:
+            self.delete_head()
+            return
+        current = self.head
+        while current.next is not None and current.next != node:
+            current = current.next
+        if current.next == node:
+            current.next = node.next
+            if node == self.tail:
+                self.tail = current
+            self.size -= 1
+
+    def sort(self):
+        if self.head is None:
+            return
+        if self.head == self.tail:
+            return
+        sorted_head = self.head
+        unsorted_head = self.head.next
+        sorted_head.next = None
+        while unsorted_head is not None:
+            node = unsorted_head
+            unsorted_head = unsorted_head.next
+            if node.val < sorted_head.data:
+                node.next = sorted_head
+                sorted_head = node
+            else:
+                current = sorted_head
+                while current.next is not None and current.next.val < node.val:
+                    current = current.next
+                node.next = current.next
+                current.next = node
+        self.head = sorted_head
+        current = self.head
+        while current.next is not None:
+            current = current.next
+        self.tail = current
+
+    def clear(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
