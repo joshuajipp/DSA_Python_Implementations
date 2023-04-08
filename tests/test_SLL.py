f9188fb34e4617e97f82b2cb3dbad4944d564f8f
joshuajipp <joshuajipp@gmail.com>
2023-04-08 12:35:29 -0600

    added tests for index insert

--- tests/test_SLL.py
+++ tests/test_SLL.py
@@ -14,18 +14,71 @@ def test_insert_head():
     assert sll.size == 2
 
 
-def test_sorted():
+def test_insert_tail():
+    sll = SinglyLinkedList()
+    sll.insertTail(Node(1))
+    assert sll.head.val == 1
+    assert sll.tail.val == 1
+    assert sll.size == 1
+    sll.insertTail(Node(2))
+    assert sll.head.val == 1
+    assert sll.tail.val == 2
+    assert sll.size == 2
+    sll.insertTail
+
+
+def test_insert():
+    sll = SinglyLinkedList()
+    sll.insert(Node(1), 0)
+    assert sll.head.val == 1
+    assert sll.tail.val == 1
+    assert sll.size == 1
+    sll.insert(Node(2), 0)
+    assert sll.head.val == 2
+    assert sll.tail.val == 1
+    assert sll.size == 2
+    sll.insert(Node(3), 1)
+    assert sll.head.val == 2
+    assert sll.tail.val == 1
+    assert sll.size == 3
+    assert sll.head.next.val == 3
+    assert sll.head.next.next.val == 1
+    sll.insert(Node(4), 3)
+    assert sll.head.val == 2
+    assert sll.tail.val == 4
+    assert sll.size == 4
+    assert sll.head.next.val == 3
+    assert sll.head.next.next.val == 1
+    assert sll.head.next.next.next.val == 4
+
+    raisedException = False
+    try:
+        sll.insert(Node(5), 5)
+    except IndexError:
+        raisedException = True
+    assert raisedException
+
+    raisedException = False
+    try:
+        sll.insert(Node(5), -1)
+    except IndexError:
+        raisedException = True
+    assert raisedException
+
+
+def test_is_sorted():
     not_sorted_sll = SinglyLinkedList()
-    not_sorted_sll.insertHead(Node(2))
-    not_sorted_sll.insertHead(Node(4))
-    not_sorted_sll.insertHead(Node(6))
-    not_sorted_sll.insertHead(Node(8))
-    not_sorted_sll.insertHead(Node(7))
+    not_sorted_sll.insertTail(Node(2))
+    not_sorted_sll.insertTail(Node(4))
+    not_sorted_sll.insertTail(Node(6))
+    not_sorted_sll.insertTail(Node(8))
+    not_sorted_sll.insertTail(Node(7))
     assert not_sorted_sll.isSorted() == False
     sorted_sll = SinglyLinkedList()
-    sorted_sll.insertHead(Node(2))
-    sorted_sll.insertHead(Node(4))
-    sorted_sll.insertHead(Node(6))
-    sorted_sll.insertHead(Node(8))
-    sorted_sll.insertHead(Node(9))
+    sorted_sll.insertTail(Node(2))
+    sorted_sll.insertTail(Node(4))
+    sorted_sll.insertTail(Node(6))
+    sorted_sll.insertTail(Node(8))
+    sorted_sll.insertTail(Node(9))
     assert sorted_sll.isSorted() == True
+    sorted_sll.print_list()
