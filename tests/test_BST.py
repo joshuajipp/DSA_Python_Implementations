4f6abfd3c4ad2738b22b424ca780e82d4a98f05b
mizybermas <113049715+mizybermas@users.noreply.github.com>
2023-04-10 12:56:46 -0600

    added delete, printInOrder and printBF tests.

--- tests/test_BST.py
+++ tests/test_BST.py
@@ -47,21 +47,21 @@ def test_insertNode():
     assert tree.root.right.left.data == 6
     assert tree.root.right.right.data == 8
 
-# def test_delete():
-#     tree = BST()
-#     tree.insert(5)
-#     tree.insert(3)
-#     tree.insert(7)
-#     tree.insert(2)
-#     tree.insert(4)
-#     tree.insert(6)
-#     tree.insert(8)
-#     tree.delete(4)
-#     assert tree.root.left.right is None
-#     tree.delete(7)
-#     assert tree.root.right.data == 8
-#     tree.delete(5)
-#     assert tree.root.data == 6
+def test_delete():
+    tree = BST()
+    tree.insert(5)
+    tree.insert(3)
+    tree.insert(7)
+    tree.insert(2)
+    tree.insert(4)
+    tree.insert(6)
+    tree.insert(8)
+    tree.delete(4)
+    assert tree.root.left.right is None
+    tree.delete(7)
+    assert tree.root.right.data == 8
+    tree.delete(5)
+    assert tree.root.data == 6
 
 def test_search():
     tree = BST()
@@ -76,31 +76,31 @@ def test_search():
     assert tree.search(7).data == 7
     assert tree.search(10) is None
 
-# def test_print_in_order(capsys):
-#     tree = BST()
-#     tree.insert(5)
-#     tree.insert(3)
-#     tree.insert(7)
-#     tree.insert(2)
-#     tree.insert(4)
-#     tree.insert(6)
-#     tree.insert(8)
-#     tree.print_in_order()
-#     captured = capsys.readouterr()
-#     assert captured.out == "2 3 4 5 6 7 8 "
+def test_printInOrder(capsys):
+    tree = BST()
+    tree.insert(5)
+    tree.insert(3)
+    tree.insert(7)
+    tree.insert(2)
+    tree.insert(4)
+    tree.insert(6)
+    tree.insert(8)
+    tree.printInOrder()
+    captured = capsys.readouterr()
+    assert captured.out == "2 3 4 5 6 7 8 "
 
-# def test_printBF():
-#     tree = BST()
-#     tree.insert(5)
-#     tree.insert(3)
-#     tree.insert(7)
-#     tree.insert(2)
-#     tree.insert(4)
-#     tree.insert(6)
-#     tree.insert(8)
-#     output = io.StringIO()
-#     sys.stdout = output
-#     tree.printBF()
-#     sys.stdout = sys.__stdout__
-#     output_str = output.getvalue()
-#     assert output_str == "5 \n3 7 \n2 4 6 8 \n"
+def test_printBF():
+    tree = BST()
+    tree.insert(5)
+    tree.insert(3)
+    tree.insert(7)
+    tree.insert(2)
+    tree.insert(4)
+    tree.insert(6)
+    tree.insert(8)
+    output = io.StringIO()
+    sys.stdout = output
+    tree.printBF()
+    sys.stdout = sys.__stdout__
+    output_str = output.getvalue()
+    assert output_str == "5 \n3 7 \n2 4 6 8 \n"
