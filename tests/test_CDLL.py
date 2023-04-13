eda0fb930a5b74cee15236c122f5c7920a333cfd
joshuajipp <joshuajipp@gmail.com>
2023-04-12 19:11:00 -0600

    added tests for insert

--- tests/test_CDLL.py
+++ tests/test_CDLL.py
@@ -18,6 +18,7 @@ def test_insertHead():
     assert cdll.head.prev.prev.prev.val == 1
     assert cdll.tail.next.val == 0
     assert cdll.tail.val == 3
+    assert cdll.size == 4
 
 
 def test_insertTail():
@@ -36,3 +37,39 @@ def test_insertTail():
     assert cdll.head.prev.prev.prev.val == 1
     assert cdll.tail.next.val == 0
     assert cdll.tail.val == 3
+    assert cdll.size == 4
+
+
+def test_insert():
+    cdll = CircularDoublyLinkedList()
+    cdll.insert(DNode(2), 0)
+    cdll.insert(DNode(4), 1)
+    cdll.insert(DNode(0), 0)
+    cdll.insert(DNode(1), 1)
+    cdll.insert(DNode(3), 3)
+    assert cdll.head.val == 0
+    assert cdll.head.next.val == 1
+    assert cdll.head.next.next.val == 2
+    assert cdll.head.next.next.next.val == 3
+    assert cdll.head.next.next.next.next.val == 4
+    assert cdll.head.next.next.next.next.next.val == 0
+    assert cdll.head.prev.val == 4
+    assert cdll.head.prev.prev.val == 3
+    assert cdll.head.prev.prev.prev.val == 2
+    assert cdll.tail.next.val == 0
+    assert cdll.tail.val == 4
+    assert cdll.size == 5
+
+    exception_raised = False
+    try:
+        cdll.insert(DNode(5), 6)
+    except IndexError:
+        exception_raised = True
+    assert exception_raised
+
+    exception_raised = False
+    try:
+        cdll.insert(DNode(5), -1)
+    except IndexError:
+        exception_raised = True
+    assert exception_raised
