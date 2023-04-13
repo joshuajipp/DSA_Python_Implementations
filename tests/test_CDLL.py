fb994a96e0d226ceff17adb9783a022d679f176e
joshuajipp <joshuajipp@gmail.com>
2023-04-12 19:02:48 -0600

    added tests for insertTail

--- tests/test_CDLL.py
+++ tests/test_CDLL.py
@@ -18,3 +18,21 @@ def test_insertHead():
     assert cdll.head.prev.prev.prev.val == 1
     assert cdll.tail.next.val == 0
     assert cdll.tail.val == 3
+
+
+def test_insertTail():
+    cdll = CircularDoublyLinkedList()
+    cdll.insertTail(DNode(0))
+    cdll.insertTail(DNode(1))
+    cdll.insertTail(DNode(2))
+    cdll.insertTail(DNode(3))
+    assert cdll.head.val == 0
+    assert cdll.head.next.val == 1
+    assert cdll.head.next.next.val == 2
+    assert cdll.head.next.next.next.val == 3
+    assert cdll.head.next.next.next.next.val == 0
+    assert cdll.head.prev.val == 3
+    assert cdll.head.prev.prev.val == 2
+    assert cdll.head.prev.prev.prev.val == 1
+    assert cdll.tail.next.val == 0
+    assert cdll.tail.val == 3
